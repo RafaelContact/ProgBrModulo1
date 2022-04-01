12c5790297cce34744e40ff3660d3532b278aca8
RafaelContact <contactrodrigues21@gmail.com>
2022-04-01 02:22:33 -0300

    igualdade

--- js/script.js
+++ js/script.js
@@ -1,28 +1,13 @@
-//  == igual a
-//  === valor igual e igual
-//  ! = não é igual
-//  !== não igual ou não igual
-//  >  maior que 
-//  < menor que 
-//  <= menor que ou igual a
-//  >= menor ou igual a 
+// &&
+// || ou
+//  negativo
 
-//  uso para comparar meus números e ele vai imprimir falso
-console.log( 2 == 3 )
+var a = true
+var b = false
 
-var a = 3
-var b = "3"
+var c = a && b
+// a pergunta que estou fazendo é se a e b são verdadeiros , este sinal é de comparação lógica boleana
+console.log(c)
 
-console.log(a == b)
-// nesse caso ele vai dizer que é igual, mesmo que um seja string e o outro numeral. Para funcionar terá que ser : 
-console.log (a === b)
-//  Com 3 iguais ele compara o número e o tipo, e número e string são diferentes
-
-console.log (a != b)
-//  olha se é diferente mas só olha o número 
-console.log (a !== b) 
-//  agora ele olha o tipo também
-
-console.log (a > b )
-//  se os números forem iguais ele vai dar falso, para maior e menor, se eu quiser um verdadeiro preciso colocar >= <=
-
+var c = a || b
+console.log(c)
