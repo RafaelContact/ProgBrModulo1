4f8c71cd509859039a02b1b7cacc4c7854edac76
RafaelContact <contactrodrigues21@gmail.com>
2022-04-01 02:10:12 -0300

    operadores boleanos

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-//  =    x = y      x = y
-// +=    x += y     x = x + y
-// -=    x -= y     x = x - y
-// *=    x *= y     x = x * y
-// /=    x /= y     x = x / y
-// %=    x %= y     x = x % y
+//  == igual a
+//  === valor igual e igual
+//  ! = não é igual
+//  !== não igual ou não igual
+//  >  maior que 
+//  < menor que 
+//  <= menor que ou igual a
+//  >= menor ou igual a 
 
-var a = 30
+//  uso para comparar meus números e ele vai imprimir falso
+console.log( 2 == 3 )
 
-// Soma ao ao valor que já tem na variável
-a += 2
+var a = 3
+var b = "3"
 
-a++ 
-a = a + 1;
-// todas essas formas são fazem a mesma operação 
-console.log(a);
+console.log(a == b)
+// nesse caso ele vai dizer que é igual, mesmo que um seja string e o outro numeral. Para funcionar terá que ser : 
+console.log (a === b)
+//  Com 3 iguais ele compara o número e o tipo, e número e string são diferentes
+
+console.log (a != b)
+//  olha se é diferente mas só olha o número 
+console.log (a !== b) 
+//  agora ele olha o tipo também
+
+console.log (a > b )
+//  se os números forem iguais ele vai dar falso, para maior e menor, se eu quiser um verdadeiro preciso colocar >= <=
+
