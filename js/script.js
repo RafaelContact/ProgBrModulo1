784ddd4ebe3b92c28ee379e9f4e240266f50de48
RafaelContact <contactrodrigues21@gmail.com>
2022-04-01 01:36:48 -0300

    simplificaçao

--- js/script.js
+++ js/script.js
@@ -1,22 +1,16 @@
-// + Adição
-//  - subtração
-//  * Multiplicação
-//  / divisão
-//  % módulo
-//  ++ incremento
-//  -- Decremento
+//  =    x = y      x = y
+// +=    x += y     x = x + y
+// -=    x -= y     x = x - y
+// *=    x *= y     x = x * y
+// /=    x /= y     x = x / y
+// %=    x %= y     x = x % y
 
-var a= 30;
+var a = 30
 
-var b = a--;
-//  nesse caso ele coloca o valor de a em b e depois decrescenta de a, então b já recebeu a e não exibe seu valor alterado
-console.log(b);
+// Soma ao ao valor que já tem na variável
+a += 2
+
+a++ 
+a = a + 1;
+// todas essas formas são fazem a mesma operação 
 console.log(a);
-
-var c = --a;
-
-console.log(c);
-// nesse caso ele decresce 1 e depois acrescenta o valor de a
-
---a; // diminui um
-++a; // decrescenta um
