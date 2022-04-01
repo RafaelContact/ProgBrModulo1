8925d2965bd935bd7da54582657bdea1856a8f67
RafaelContact <contactrodrigues21@gmail.com>
2022-04-01 00:57:31 -0300

    operadores aritimeticos

--- js/script.js
+++ js/script.js
@@ -11,6 +11,16 @@ var a = 10;
 var b = 15; 
 
 var c = 2 + 3; 
-//  eu poderia atribuir um resultado, um número e uma string
+var c = 2 - 3;
+var c = a / b;
+var c = a * b;
+var c = a % b; // Retorna o valor do resto da divisão, ( apenas números reais)
+
+//  Em js eu só tenho um tipo de numero , que é o number, em outras linguagens eu tenho números inteiros, números reais e etc.
 
 console.log(c);
+
+var a= 30;
+
+a--; // diminui um
+a++; // decrescenta um
