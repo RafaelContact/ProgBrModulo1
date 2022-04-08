a2e2832ced6c3e2563c93a9c17fd041f8fe93996
RafaelContact <contactrodrigues21@gmail.com>
2022-04-08 13:36:47 -0300

    transformando str em número parseInt

--- js/str.js
+++ js/str.js
@@ -2,7 +2,7 @@ var a = '3'
 
 var b = '5'
 
-var c = a + b
+var c = parseInt(a) + parseInt(b)
 // Agora ao invez de 8 eu vou ter 35 pois os números foram concatenados
 //  ele entende que usando string e soma eu quero concatenar
 console.log(c)
