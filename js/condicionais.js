65abf3344aa423b4edc5b14863df6de59b08d7ee
RafaelContact <contactrodrigues21@gmail.com>
2022-04-08 18:33:50 -0300

    venda de bebidas

--- js/condicionais.js
+++ js/condicionais.js
@@ -1,14 +1,10 @@
-var idade = 19
+var idade = 29
 
-if (idade >= 35){
+if (idade >= 18 || idade > 70){
     console.log("pode") 
     console.log('Qual o seu pedido ?')
 }
-// Quando eu executo mais de uma linha de comandos condicionais vou precisar de um colchete para demarcar um bloco
-else if (idade >= 18)  {
-    console.log('pode')
-    console.log('Mostre a identidade')
-}
+// 
 else{
     console.log('não pode')
     console.log ('volte futuramente')
