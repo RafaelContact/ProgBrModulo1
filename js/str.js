ad4f4fced514b07b6365006e4022f9da522a9890
RafaelContact <contactrodrigues21@gmail.com>
2022-04-08 13:15:56 -0300

    transformando string em número

--- js/str.js
+++ js/str.js
@@ -1,6 +1,6 @@
- var numero = 8
+ var numero = "8" * 2
  str += "valor qualquer" + numero
-//  Sempre que estiver preto é equivalente a uma str, sempre que estiver azul é um número
+//  As strings só conseguem trabalhar com a adição caso eu tente multiplicar uma string da um erro, mas se ela for só um número do tipo string ele se transforma em um número deixando de ser uma string.
  
 console.log(numero);
 console.log(str);
