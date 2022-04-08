ae8719cfc6e8e827c4765731369e2fd770cea7be
RafaelContact <contactrodrigues21@gmail.com>
2022-04-08 13:39:12 -0300

    parseFloat para números reais

--- js/str.js
+++ js/str.js
@@ -1,8 +1,8 @@
-var a = '3'
+var a = '3.5'
 
 var b = '5'
 
-var c = parseInt(a) + parseInt(b)
+var c = parseFloat(a) + parseFloat(b)
 // Agora ao invez de 8 eu vou ter 35 pois os números foram concatenados
 //  ele entende que usando string e soma eu quero concatenar
 console.log(c)
