be6587fdc7b5127324e66c3521e79659c4ee43c6
RafaelContact <contactrodrigues21@gmail.com>
2022-04-08 16:03:15 -0300

    estrutura condicional aninhada

--- js/condicionais.js
+++ js/condicionais.js
@@ -1,11 +1,15 @@
 var idade = 19
 
-if (idade >= 18){
+if (idade >= 35){
     console.log("pode") 
     console.log('Qual o seu pedido ?')
 }
 // Quando eu executo mais de uma linha de comandos condicionais vou precisar de um colchete para demarcar um bloco
-else {
+else if (idade >= 18)  {
+    console.log('pode')
+    console.log('Mostre a identidade')
+}
+else{
     console.log('não pode')
-    console.log('volte futuramente')
+    console.log ('volte futuramente')
 }
