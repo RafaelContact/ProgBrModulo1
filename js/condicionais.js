3743c70d9ed5a1c52354343458bd039b6a09adbc
RafaelContact <contactrodrigues21@gmail.com>
2022-04-08 15:59:49 -0300

    estutura condicional composta

--- js/condicionais.js
+++ js/condicionais.js
@@ -1,7 +1,11 @@
 var idade = 19
 
-if (idade >= 18)
+if (idade >= 18){
     console.log("pode") 
-
-else 
+    console.log('Qual o seu pedido ?')
+}
+// Quando eu executo mais de uma linha de comandos condicionais vou precisar de um colchete para demarcar um bloco
+else {
     console.log('não pode')
+    console.log('volte futuramente')
+}
