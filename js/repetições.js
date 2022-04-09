ec1767a0cf420621c769a280d06e9b92c1ce4f6f
RafaelContact <contactrodrigues21@gmail.com>
2022-04-09 00:33:43 -0300

    laço de repetição

---
+++ js/repetições.js
@@ -0,0 +1,8 @@
+var numero = 5;
+
+for(var vez = 0; vez < numero ; vez++)
+//  vez indica a quantidade de vezes que a repetição está
+// Com a vez menor do que a variável número ela será executada. 
+// TUDO É SEPARADO POR UM PONTO E VIRGURLA ao final é acrescentado mais 1 com o operador ++
+
+// esse laço será mantido até que seja maior que 5
